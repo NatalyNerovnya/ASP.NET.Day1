aef1d098a6e7e8514d805bfd0f70346aed6ccecb
Наташа Неровня <natanerovnja@gmail.com>
2016-03-13 17:58:09 +0300

    Add method of Newton

---
+++ Root/CountRoot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Root
+{
+    /// <summary>
+    /// Сalculates the root of the n-th degree of the value by Newton's method with the specified accuracy
+    /// </summary>
+    public class CountRoot
+    {
+        /// <summary>
+        /// Сalculates the root of the n-th degree of the value by Newton's method with the specified accuracy
+        /// </summary>
+        /// <param name="val">The base</param>
+        /// <param name="deg">Degree of root</param>
+        /// <param name="acc">Accuracy</param>
+        /// <returns></returns>
+        public static double CustomRoot(double val, int deg, double acc = 0.0001)
+        {
+            if(val == 0.0 || deg == 0.0 || acc == 0)
+                throw new ArgumentException();
+            if(val < 0)
+                throw new ArgumentException();
+            
+            double x = 1;
+            double xNext = 0;
+
+            while (true)
+            {
+                xNext = ((double) 1/deg)*((deg - 1)*x + val/Math.Pow(x, deg - 1));
+                if(Math.Abs(Math.Pow(xNext, deg)-val) < acc)
+                    return xNext;
+                x = xNext;
+            }
+        }
+}
+}
---
+++ RootTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Root;
+
+namespace RootTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 16;
+            int n = 3;
+            
+            Console.WriteLine("16 ^ 1/3: \nMath.Pow: {0} \nRoot: {1}", Math.Pow(x, (double)1 / n), CountRoot.CustomRoot(x, n));
+            Console.ReadLine();
+        }
+    }
+}
