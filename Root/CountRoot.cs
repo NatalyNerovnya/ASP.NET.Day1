88a60cf1776c411062ad97cf4ba6e2b5d7f25fd9
Наташа Неровня <natanerovnja@gmail.com>
2016-04-03 10:51:17 +0300

    Add check on negative value

--- Root/CountRoot.cs
+++ Root/CountRoot.cs
@@ -22,9 +22,7 @@ namespace Root
         /// <returns></returns>
         public static double CustomRoot(double val, int deg, double acc = 0.0001)
         {
-            if (val == 0.0 || deg == 0 || acc == 0 || val == Double.NaN)
-                throw new ArgumentException();
-            if (val < 0)
+            if (val == 0.0 || deg == 0 || acc == 0 || val == Double.NaN || val < 0)
                 throw new ArgumentException();
             if (deg < 0)
                 return (double)1 / CustomRoot(val, Module(deg), acc);
